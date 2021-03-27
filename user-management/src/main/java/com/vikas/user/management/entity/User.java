417d43749f813ede65cb7fe7c194bfcbff083680
vikas <moganevikas@gmail.com>
2021-03-28 00:40:35 +0530

    getter setter created for user

--- user-management/src/main/java/com/vikas/user/management/entity/User.java
+++ user-management/src/main/java/com/vikas/user/management/entity/User.java
@@ -15,5 +15,35 @@ public class User
 	private String lastName;
 	private Integer age;
 	private Date createdDate;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
 	
 }
