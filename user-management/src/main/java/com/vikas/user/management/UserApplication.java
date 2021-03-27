8b0d4dcd946d569ecb053b7d4b18906b39cbd58c
vikas <moganevikas@gmail.com>
2021-03-27 23:19:12 +0530

    spring boot hello world example with eclipse maven project completed task--first stage

--- user-management/src/main/java/com/vikas/user/management/UserApplication.java
+++ user-management/src/main/java/com/vikas/user/management/UserApplication.java
@@ -1,13 +1,16 @@
 package com.vikas.user.management;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class UserApplication 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello world completed first step!" );
     }
 }
